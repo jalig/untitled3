b7ef753b4c69e0d264f78fec1cd2c95d3a282fb9
jalig <117361652+jalig@users.noreply.github.com>
2023-02-15 17:02:40 +0400

    done

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,71 +1,170 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 public class MyArrayList implements StringList {
+    private final int DEFAULT_CAPACITY = 10;
+    private String[] list;
+    private int size;
+
+    public MyArrayList(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Capacity < 0");
+        } else if (capacity == 0) {
+            list = new String[DEFAULT_CAPACITY];
+        } else {
+            list = new String[capacity];
+        }
+    }
+
+    public MyArrayList() {
+        list = new String[DEFAULT_CAPACITY];
+    }
+
     @Override
     public String add(String item) {
-        return null;
+        if (list[list.length - 1] != null) {
+            String[] newList = new String[list.length * 2];
+            System.arraycopy(list, 0, newList, 0, list.length);
+            list = newList;
+        }
+
+        return list[size++] = item;
     }
 
     @Override
     public String add(int index, String item) {
-        return null;
+        if (list[list.length - 1] != null) {
+            String[] newList = new String[list.length * 2];
+            System.arraycopy(list, 0, newList, 0, index);
+            newList[index] = item;
+            System.arraycopy(list, index, newList, index + 1, list.length - index);
+            list = newList;
+            size++;
+        } else {
+            String[] newList = new String[list.length];
+            System.arraycopy(list, 0, newList, 0, index);
+            newList[index] = item;
+            System.arraycopy(list, index, newList, index + 1, list.length - (index + 1));
+            list = newList;
+            size++;
+        }
+        return item;
     }
 
     @Override
     public String set(int index, String item) {
-        return null;
+        checkExp(index);
+        return list[index] = item;
     }
 
     @Override
     public String remove(String item) {
-        return null;
+        if (!contains(item)) {
+            throw new IllegalArgumentException("Ошибка удаления");
+        }
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != null && list[i].equals(item)) {
+                System.arraycopy(list, i + 1, list, i, list.length - i - 1);
+                size--;
+            }
+        }
+        return item;
     }
 
     @Override
     public String remove(int index) {
-        return null;
+        checkExp(index);
+
+        if (list[index] == null) {
+            throw new IndexOutOfBoundsException("Элемента нет :(");
+        }
+
+        String string = list[index];
+        System.arraycopy(list, index + 1, list, index, list.length - index - 1);
+        size--;
+        return string;
     }
 
     @Override
     public boolean contains(String item) {
+        for (String s : list) {
+            if (s != null && s.equals(item)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int indexOf(String item) {
-        return 0;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != null && list[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(String item) {
-        return 0;
+        for (int i = list.length - 1; i >= 0; i--) {
+            if (list[i] != null && list[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public String get(int index) {
-        return null;
+        checkExp(index);
+        if (list[index] == null) {
+            throw new IndexOutOfBoundsException("Элемента нет :(");
+        }
+        return list[index];
     }
 
     @Override
     public boolean equals(StringList otherList) {
-        return false;
+        if (otherList == null) {
+            throw new IllegalArgumentException("Нельзя передавать null");
+        }
+        if (size != otherList.size()) return false;
+        for (int i = 0; i < size; i++) {
+            if (list[i] != otherList.get(i)) return false;
+        }
+        return true;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void clear() {
-
+        size = 0;
+        list = new String[size];
     }
 
     @Override
     public String[] toArray() {
-        return new String[0];
+        return Arrays.copyOf(list, size);
+    }
+
+    private void checkExp(int index) {
+        if (index < 0 || index >= list.length) {
+            throw new ArrayIndexOutOfBoundsException("Неверный индекс");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(list, size));
     }
 }
